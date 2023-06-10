71ae120b7d43efb9ceb79597edec3878c4d7aba8
Banber228 <134782901+Banber228@users.noreply.github.com>
2023-06-10 14:27:06 +0300

    update GameSprite and add class Player

--- chypapi mynana/ping pong.py
+++ chypapi mynana/ping pong.py
@@ -1,17 +1,44 @@
 from pygame import*
-
 back=(255,200,100)
 width=700
 height=500
+class GameSptite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,player_speed,widht,height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+     def update_l(self):
+        keys = key.get_pressed()
+        if keys [K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+window=display.set_mode((width,height))
+window.fill(back)
+win_height=500
+win_width=700
 
-
-class GameSptite():
-    def __init__(self):
-        self.x=100
 mw=display.set_mode((width,height))
 mw.fill(back)
 
+
+
+
+
 cloak=time.Clock()
+ball = GameSprite('tenis-ball.png', 200, 200, 4, 50, 50)
 
 game_over=False
 
